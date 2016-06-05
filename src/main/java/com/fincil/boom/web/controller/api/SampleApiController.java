ad3b2796debc31945c944f68a8995a24c1035c5b
wooklym <wooklym@gmail.com>
2016-06-06 00:01:23 +0900

    remove legacy

--- src/main/java/com/fincil/boom/web/controller/api/SampleApiController.java
+++
@@ -1,8 +0,0 @@
-package com.fincil.boom.web.controller.api;
-
-import com.fincil.boom.services.model.SampleModel;
-
-//@Controller
-//@RequestMapping("api/..")
-public class SampleApiController extends GenericApiController<SampleModel> {
-}
--- src/main/java/com/fincil/boom/web/initializer/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.fincil.boom.web.initializer;
-
-import org.springframework.core.annotation.Order;
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-@Order(2)
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
